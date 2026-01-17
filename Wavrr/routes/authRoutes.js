e2d6776a628f097721725464c8217bda80706293
ONOWONI <danielonownoi1@gmail.com>
2026-01-17 12:40:40 +0100

    added the full routes

--- Wavrr/routes/authRoutes.js
+++ Wavrr/routes/authRoutes.js
@@ -8,12 +8,11 @@ app.use(express.urlencoded());
 
 
 // register user
+
+// full routes are /api/auth/...
 router.post('/register', userRegister)
 router.post('/artist-register', artistRegister)
 router.post('/login', userLogin)
 router.post('/artist-login', artistLogin)
-router.get('/register', (req, res) => {
-    console.log("howdy")
-    return res.status(200).json({success: true, message: "All fields are required"});
-})
+
 export default router
