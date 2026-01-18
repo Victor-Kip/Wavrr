a732e5839c5200b82e0d4a6bfd6db2006de505e9
ONOWONI <danielonownoi1@gmail.com>
2026-01-18 14:19:29 +0100

    connected to external database

--- Wavrr/config/db.js
+++ Wavrr/config/db.js
@@ -8,17 +8,17 @@ const username= process.env.DB_USERNAME
 const password= process.env.DB_PASSWORD
 const host= process.env.DB_HOST
 const port= process.env.DB_PORT
+const dbUrl= process.env.DB_URL
 
-const db = new Sequelize(
-    database, username, password,
-    {
-        dialect: 'postgres',
-        host: host,
-        port: port,
-    }
-)
+// const db = new Sequelize(
+//     database, username, password,
+//     {
+//         dialect: 'postgres',
+//         host: host,
+//     }
+// )
 
-// const db = new Sequelize(`postgres://${username}:${password}@${host}:${port}/${database}`);
+const db = new Sequelize(`${dbUrl}`);
 export default db
 
 
